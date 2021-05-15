29637340e01111804d46535e89b8f00f8a76a650
Pratik <kpratik217@gmail.com>
2021-05-15 20:54:18 +0530

    adding viewsets t provide

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -5,6 +5,7 @@ from rest_framework import viewsets
 
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
 
 
 
