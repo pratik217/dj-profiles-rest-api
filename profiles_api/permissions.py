9b6b02f41a651793da3b68d3ce67f7864ccbc272
Pratik <kpratik217@gmail.com>
2021-05-16 10:36:41 +0530

    Adde dlogin api

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -8,4 +8,13 @@ class UpdateOwnProfile(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.id  == request.user.id
-        
+
+
+class UpdateOwnStatus(permissions.BasePermission):
+    """Alloow user to update thoer own status"""
+    def has_object_permission(self, request, view, obj):
+        """Chec the user trtyin gyo update thoer own statuas"""
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.user_profile.id==request.user.id
