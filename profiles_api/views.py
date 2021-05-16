437c5a5e424e21af6a344611a05e6738f7033668
Pratik <kpratik217@gmail.com>
2021-05-16 08:30:04 +0530

    Adde dlogin api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -6,7 +6,7 @@ from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
 from rest_framework.authtoken.views import ObtainAuthToken
-
+from rest_framework.settings import api_settings
 
 
 from profiles_api import serializers
@@ -121,3 +121,7 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
     filter_backends=(filters.SearchFilter,)
     search_fields= ('name', 'email',)
+
+class UserLoginApiView(ObtainAuthToken):
+    """HAndle creatin authentication tokens"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
